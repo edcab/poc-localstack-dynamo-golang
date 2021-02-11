5ab5a59231b51c06eb96c20a6fefded43bc2168c
Edwin Cabezas <ing.edwincabezas@gmail.com>
2021-02-10 19:42:44 -0500

    implementacion acceso a dynamoDB desde ambiente local

---
+++ usecase/getparameter.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"poc-localstack-dynamo-golang/domain/dto"
+	"poc-localstack-dynamo-golang/domain/entities"
+	"poc-localstack-dynamo-golang/domain/service"
+)
+
+type Save interface {
+	Save(dto dto.ParameterDTO) error
+}
+
+type SaveImpl struct {
+}
+
+func NewUseCaseSave() Save {
+	return &SaveImpl{
+	}
+}
+
+func (s SaveImpl) Save(dto dto.ParameterDTO) error {
+	saveService := service.NewSaveService()
+
+	parameter := entities.Parameter{
+		Key:   dto.Key,
+		Value: dto.Value,
+	}
+
+	err := saveService.Save(parameter)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
